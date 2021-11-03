9b2cfbc73a6d2108ad109c29dd938ac910c5f2a4
Spyro1001 <30807727+Spyro1001@users.noreply.github.com>
2021-11-02 17:55:44 -0700

    Initial commit

---
+++ src/main/java/com/example/ElementNotFoundException.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class ElementNotFoundException extends Exception {
+
+    public ElementNotFoundException() {
+        super();
+    }
+
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+
+    public ElementNotFoundException(String message, Throwable t) {
+        super(message, t);
+    }
+
+    public ElementNotFoundException(Throwable t) {
+        super(t);
+    }
+
+}
